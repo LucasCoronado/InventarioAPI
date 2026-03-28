d58b744453c2d69f3e476f8eb9351aaa55b4f79a
LucasCoronado <coronadolucasm@gmail.com>
2026-03-27 21:34:43 -0300

    chore: instalar y configurar Swagger UI para pruebas visuales

--- InventarioAPI/Program.cs
+++ InventarioAPI/Program.cs
@@ -17,12 +17,16 @@ namespace InventarioAPI
 
             builder.Services.AddScoped<RepuestoRepository>();
 
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
-                app.MapOpenApi();
+                app.UseSwagger();   
+                app.UseSwaggerUI();
             }
 
             app.UseHttpsRedirection();
